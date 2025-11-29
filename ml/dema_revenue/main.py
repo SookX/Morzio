c7c1b60b852da1320e824a656cb526b1b61e1e79
Vasil Vasilev <vagrivas08@gmail.com>
2025-11-29 12:14:47 +0200

    Dema revenue estimation

---
+++ ml/dema_revenue/main.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+
+def calculate_dobule_ema(data, alpha1=0.5, alpha2=0.3, windows_size = 3):
+    
+    """
+    Calculate Double Exponential Moving Average (DEMA) for the given data.
+    
+    Parameters:
+    data (list or np.array): Input data series.
+    alpha1 (float): Smoothing factor for the first EMA.
+    alpha2 (float): Smoothing factor for the second EMA.
+    
+    Returns:
+    np.array: DEMA values.
+    """
+
+    ema1 = np.zeros(len(data))
+    ema2 = np.zeros(len(data))
+    
+    ema1[0] = data[0]
+    for t in range(1, len(data)):
+        ema1[t] = alpha1 * data[t] + (1 - alpha1) * ema1[t-1]
+    
+    ema2[0] = ema1[0]
+    for t in range(1, len(data)):
+        ema2[t] = alpha2 * ema1[t] + (1 - alpha2) * ema2[t-1]
+    
+    dema = 2 * ema1 - ema2
+    return dema
+
+if __name__ == "__main__":
+    incomes = [1500, 1800, 1500, 1600, 1400, 1300, 1500, 1700, 1500] 
+    ewa_incomes = calculate_dobule_ema(incomes)
+    print("Original Incomes: ", incomes)
+    print("Double EMA Incomes: ", ewa_incomes)
