564d5c71639c951af3f2c52b294666bb6cb937d2
Krum Sultov <krsultov@gmail.com>
2025-11-30 01:07:36 +0200

    plaid service

---
+++ server/src/main/java/com/morzio/server/services/PlaidService.java
@@ -0,0 +1,75 @@
+package com.morzio.server.services;
+
+import com.plaid.client.request.PlaidApi;
+import com.plaid.client.model.*;
+import org.springframework.stereotype.Service;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Service
+public class PlaidService {
+
+    private final PlaidApi plaidApi;
+
+    public PlaidService(PlaidApi plaidApi) {
+        this.plaidApi = plaidApi;
+    }
+
+    public String createLinkToken(String clientUserId) throws IOException {
+        LinkTokenCreateRequestUser user = new LinkTokenCreateRequestUser()
+                .clientUserId(clientUserId);
+
+        LinkTokenCreateRequest request = new LinkTokenCreateRequest()
+                .user(user)
+                .clientName("Morzio")
+                .products(Arrays.asList(Products.TRANSACTIONS, Products.PAYMENT_INITIATION))
+                .countryCodes(Collections.singletonList(CountryCode.GB))
+                .language("en");
+
+        Response<LinkTokenCreateResponse> response = plaidApi.linkTokenCreate(request).execute();
+
+        if (!response.isSuccessful()) {
+            throw new RuntimeException("Failed to create link token: "
+                    + (response.errorBody() != null ? response.errorBody().string() : "Unknown error"));
+        }
+
+        return response.body().getLinkToken();
+    }
+
+    public String exchangePublicToken(String publicToken) throws IOException {
+        ItemPublicTokenExchangeRequest request = new ItemPublicTokenExchangeRequest()
+                .publicToken(publicToken);
+
+        Response<ItemPublicTokenExchangeResponse> response = plaidApi.itemPublicTokenExchange(request).execute();
+
+        if (!response.isSuccessful()) {
+            throw new RuntimeException("Failed to exchange public token: "
+                    + (response.errorBody() != null ? response.errorBody().string() : "Unknown error"));
+        }
+
+        return response.body().getAccessToken();
+    }
+
+    public TransactionsGetResponse getTransactions(String accessToken) throws IOException {
+        LocalDate startDate = LocalDate.now().minusDays(90);
+        LocalDate endDate = LocalDate.now();
+
+        TransactionsGetRequest request = new TransactionsGetRequest()
+                .accessToken(accessToken)
+                .startDate(startDate)
+                .endDate(endDate);
+
+        Response<TransactionsGetResponse> response = plaidApi.transactionsGet(request).execute();
+
+        if (!response.isSuccessful()) {
+            throw new RuntimeException("Failed to get transactions: "
+                    + (response.errorBody() != null ? response.errorBody().string() : "Unknown error"));
+        }
+
+        return response.body();
+    }
+}
