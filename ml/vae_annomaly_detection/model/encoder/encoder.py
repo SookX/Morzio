aff790d1dff67f10941e0ef20c107ffcd4f3aee6
Vasil Vasilev <vagrivas08@gmail.com>
2025-11-29 14:57:36 +0200

    VAE improvement

--- ml/vae_annomaly_detection/model/encoder/encoder.py
+++ ml/vae_annomaly_detection/model/encoder/encoder.py
@@ -6,13 +6,18 @@ class Encoder(nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim):
         super(Encoder, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, latent_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim * 2)
+        self.fc3 = nn.Linear(hidden_dim * 2, latent_dim)
+        self.dropout = nn.Dropout(0.1)
 
         self.activation = nn.SiLU()
 
     def forward(self, x):
         x = self.activation(self.fc1(x))
-        x = self.fc2(x)
+        x = self.dropout(x)
+        x = self.activation(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
         return x
     
 if __name__ == "__main__":
