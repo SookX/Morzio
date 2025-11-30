6b5c28f23064f319ac6737e5d0ef3a6ed0e3f5d9
anastassow <127148449+anastassow@users.noreply.github.com>
2025-11-30 13:41:31 +0200

    docker

--- server/src/main/java/com/morzio/server/ServerApplication.java
+++ server/src/main/java/com/morzio/server/ServerApplication.java
@@ -4,15 +4,14 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		
-		Dotenv dotenv = Dotenv.load();
-    	dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
